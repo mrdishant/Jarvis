4329c2a665a136aa2d3be6d6ccba3e53de3c64f6
Dishant Mahajan <mr.dishantmahajan@gmail.com>
2018-01-23 22:07:01 +0530

    Submission

--- app/src/main/java/com/nearur/jarvis/Music.java
+++ app/src/main/java/com/nearur/jarvis/Music.java
@@ -4,11 +4,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnBufferingUpdateListener;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -21,7 +19,7 @@ import com.vstechlab.easyfonts.EasyFonts;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Music extends AppCompatActivity implements View.OnClickListener , View.OnTouchListener,OnBufferingUpdateListener {
+public class Music extends AppCompatActivity implements View.OnClickListener  {
 
     ImageButton play,previous,next;
     ImageView imageView;
@@ -37,8 +35,6 @@ public class Music extends AppCompatActivity implements View.OnClickListener , V
     private SwagPoints seekBarProgress;
 
 
-    private int mediaFileLengthInMilliseconds; // this value contains the song duration in milliseconds. Look at getDuration() method in MediaPlayer class
-
     private final Handler handler = new Handler();
 
     @Override
@@ -52,9 +48,26 @@ public class Music extends AppCompatActivity implements View.OnClickListener , V
         txtsong.setTypeface(EasyFonts.caviarDreams(Music.this));
 
         seekBarProgress=(SwagPoints)findViewById(R.id.seekbar_point);
-        seekBarProgress.setPoints(99);
-        seekBarProgress.setOnTouchListener(this);
-        mediaPlayer.setOnBufferingUpdateListener(this);
+
+
+        seekBarProgress.setOnSwagPointsChangeListener(new SwagPoints.OnSwagPointsChangeListener() {
+            @Override
+            public void onPointsChanged(SwagPoints swagPoints, int i, boolean b) {
+                if(mediaPlayer != null && b){
+                    mediaPlayer.seekTo(i * 1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SwagPoints swagPoints) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SwagPoints swagPoints) {
+
+            }
+        });
 
 
 
@@ -100,7 +113,6 @@ public class Music extends AppCompatActivity implements View.OnClickListener , V
 
     void image(){
         mediaMetadataRetriever.setDataSource(path.get(idx));
-        mediaFileLengthInMilliseconds=mediaPlayer.getDuration();
         primarySeekBarProgressUpdater();
         art=mediaMetadataRetriever.getEmbeddedPicture();
         if(art!=null){
@@ -169,30 +181,22 @@ public class Music extends AppCompatActivity implements View.OnClickListener , V
         }
     }
 
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        if(mediaPlayer.isPlaying()){
-            SwagPoints sb = (SwagPoints)view;
-            int playPositionInMillisecconds = (mediaFileLengthInMilliseconds / 100) * sb.getPoints();
-            mediaPlayer.seekTo(playPositionInMillisecconds);
-        }
-        return false;
-    }
-
-    @Override
-    public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
-        seekBarProgress.setPoints(i);
-    }
-
     private void primarySeekBarProgressUpdater() {
-        seekBarProgress.setPoints((int)(((float)mediaPlayer.getCurrentPosition()/mediaFileLengthInMilliseconds)*100)); // This math construction give a percentage of "was playing"/"song length"
+
         if (mediaPlayer.isPlaying()) {
-            Runnable notification = new Runnable() {
+
+            seekBarProgress.setMax((mediaPlayer.getDuration()/1000)+1);
+            Music.this.runOnUiThread(new Runnable() {
+
+                @Override
                 public void run() {
-                    primarySeekBarProgressUpdater();
+                    if(mediaPlayer != null){
+                        int mCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
+                        seekBarProgress.setPoints(mCurrentPosition);
+                    }
+                    handler.postDelayed(this, 1000);
                 }
-            };
-            handler.postDelayed(notification,1000);
+            });
         }
     }
 }
